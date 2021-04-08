f6a6d3753ba75990ff4af78a3f331f2c590dd244
jesusism <thejesusism@gmail.com>
2021-04-08 22:32:56 +0900

    Initial commit

---
+++ src/main/java/weawe/praybook/PraybookApplication.java
@@ -0,0 +1,14 @@
+package weawe.praybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraybookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PraybookApplication.class, args);
+    }
+
+}
+A
---
+++ src/test/java/weawe/praybook/PraybookApplicationTests.java
@@ -0,0 +1,13 @@
+package weawe.praybook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraybookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
