b6b1fae27fd6e56cb13ec3ca0a2dd8863c40b160
jesusism <thejesusism@gmail.com>
2021-04-12 00:09:03 +0900

    gitignore 추가

---
+++ src/main/java/weawe/praybook/service/PostsService.java
@@ -0,0 +1,36 @@
+package weawe.praybook.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import weawe.praybook.domain.posts.Posts;
+import weawe.praybook.domain.posts.PostsRepository;
+import weawe.praybook.web.dto.PostsResponseDto;
+import weawe.praybook.web.dto.PostsSaveRequestDto;
+import weawe.praybook.web.dto.PostsUpdateRequestDto;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+        return new PostsResponseDto(entity);
+    }
+}
