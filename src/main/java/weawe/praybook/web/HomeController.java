622899f7ff2fc3e3bd2021a8675c82bc25b9cfe8
jesusism <thejesusism@gmail.com>
2021-04-14 14:36:25 +0900

    home 화면 추가

--- src/main/java/weawe/praybook/web/HomeController.java
+++ src/main/java/weawe/praybook/web/HomeController.java
@@ -2,11 +2,11 @@ package weawe.praybook.web;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 @Slf4j
 public class HomeController {
     @GetMapping(value = "/")
