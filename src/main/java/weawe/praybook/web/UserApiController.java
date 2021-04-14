5ead087547639c90b8df2ba35efb9b78f76c1a89
jesusism <thejesusism@gmail.com>
2021-04-14 23:41:39 +0900

    템플릿엔진 타임리프에서 - Mustache 교체

--- src/main/java/weawe/praybook/web/UserApiController.java
+++ src/main/java/weawe/praybook/web/UserApiController.java
@@ -1,6 +1,7 @@
 package weawe.praybook.web;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import weawe.praybook.service.UserService;
 import weawe.praybook.web.dto.UserResponseDto;
@@ -9,27 +10,32 @@ import weawe.praybook.web.dto.UserUpdateRequestDto;
 
 @RequiredArgsConstructor
 @RestController
+@Slf4j
 public class UserApiController {
 
     private final UserService userService;
 
     @PostMapping("/api/user")
     public Long save(@RequestBody UserSaveRequestDto requestDto) {
+        log.info("UserApiController-save");
         return userService.save(requestDto);
     }
 
     @PutMapping("/api/user/{id}")
     public Long update(@PathVariable Long id, @RequestBody UserUpdateRequestDto requestDto) {
+        log.info("UserApiController-update");
         return userService.update(id, requestDto);
     }
 
     @GetMapping("/api/user/{id}")
     public UserResponseDto findById (@PathVariable Long id) {
+        log.info("UserApiController-findById");
         return userService.findById(id);
     }
 
     @DeleteMapping("/api/user/{id}")
     public Long delete(@PathVariable Long id) {
+        log.info("UserApiController-delete");
         userService.delete(id);
         return id;
     }
