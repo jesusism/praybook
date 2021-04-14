37faee162e3c484b468aa1934d20efcee5f882dd
jesusism <thejesusism@gmail.com>
2021-04-14 14:02:45 +0900

    타임리프로 변경

--- src/main/java/weawe/praybook/web/IndexController.java
+++ src/main/java/weawe/praybook/web/IndexController.java
@@ -4,22 +4,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import weawe.praybook.service.PostsService;
+import weawe.praybook.service.PrayService;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-    private final PostsService postsService;
+    private final PrayService prayService;
 
-    @GetMapping("/")
+    @GetMapping("/index")
     public String index(Model model) {
-        model.addAttribute("posts", postsService.findAllDesc());
+        model.addAttribute("pray", prayService.findAllDesc());
         return "index";
     }
 
-    @GetMapping("/posts/save")
-    public String postsSave() {
-        return "posts-save";
+    @GetMapping("/pray/save")
+    public String praySave() {
+        return "pray-save";
     }
 }
