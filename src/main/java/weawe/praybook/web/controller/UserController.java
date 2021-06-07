5e2e5389b8a79570cba3ccacb3638609d1d0d78b
jesusism <thejesusism@gmail.com>
2021-06-07 13:10:10 +0900

    sign up, sign in

---
+++ src/main/java/weawe/praybook/web/controller/UserController.java
@@ -0,0 +1,32 @@
+package weawe.praybook.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import weawe.praybook.service.UserService;
+import weawe.praybook.web.dto.UserSaveRequestDto;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RequiredArgsConstructor
+@Controller
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/user")
+    public String signup(UserSaveRequestDto userSaveRequestDto) { // 회원 추가
+        userService.save(userSaveRequestDto);
+        return "redirect:/login";
+    }
+
+    // 추가
+    @GetMapping(value = "/logout")
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
+    }
+}
